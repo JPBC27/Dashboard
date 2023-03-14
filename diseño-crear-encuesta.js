021c09f0b586c58e4dd636c0612697c156ae0634
Jean Paul <jeanpaulbenitescoico@gmail.com>
2023-03-14 13:46:06 -0500

    envio4

--- diseño-crear-encuesta.js
+++ diseño-crear-encuesta.js
@@ -1,8 +1,39 @@
 const body= document.querySelector("body"),
       sidebar=body.querySelector(".sidebar-2"),
-      toggle=body.querySelector(".bi-caret-right");
-
+      toggle=body.querySelector(".bi-caret-right"),
+      submenu=document.querySelector(".sub-menu");
 
       toggle.addEventListener("click",()=>{
         sidebar.classList.toggle("cerrar"); 
       });
+
+const links=document.querySelectorAll('.nav-link2');
+
+links.forEach(element => {
+  element.addEventListener("click",function(e) {
+ 
+      toggle.classList.toggle("hidden-arrow")
+      // console.log(e)
+    
+  })
+
+
+  // element.addEventListener("mouseout",function(e) {
+  //     toggle.classList.remove("hidden-arrow")
+  //     // console.log(e)
+  // })
+});
+
+const primero=document.querySelector('.primer');
+
+primero.addEventListener("click",()=>{
+  submenu.classList.toggle("activate-sub-menu")
+
+
+})
+
+
+
+
+
+
