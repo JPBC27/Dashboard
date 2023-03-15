a69d33690b7b41a700e90809de105ef79cea19f4
Jean Paul <jeanpaulbenitescoico@gmail.com>
2023-03-15 17:19:49 -0500

    envio03/15

--- diseño-crear-encuesta.js
+++ diseño-crear-encuesta.js
@@ -1,7 +1,8 @@
 const body= document.querySelector("body"),
       sidebar=body.querySelector(".sidebar-2"),
       toggle=body.querySelector(".bi-caret-right"),
-      submenu=document.querySelector(".sub-menu");
+      submenu=document.querySelector(".sub-menu"),
+      submenu1=document.querySelector(".sub-menu-1");
 
       toggle.addEventListener("click",()=>{
         sidebar.classList.toggle("cerrar"); 
@@ -9,28 +10,103 @@ const body= document.querySelector("body"),
 
 const links=document.querySelectorAll('.nav-link2');
 
-links.forEach(element => {
-  element.addEventListener("click",function(e) {
+
+// const links=document.querySelectorAll('.nav-link2');
+// const menu=body.querySelectorAll(".menu2");
+
+
+// menu.forEach(element => {
+//   element.addEventListener("mouseover",function(e) {
  
-      toggle.classList.toggle("hidden-arrow")
-      // console.log(e)
+//       toggle.classList.toggle("hidden-arrow");
+      
     
+//   })
+
+  
+//   element.addEventListener("mouseout",function(e) {
+//       toggle.classList.remove("hidden-arrow")
+      
+//   })
+// });
+
+// ===============================================
+// funciones para la barra lateral
+// ===============================================
+links.forEach(element => {
+  // console.log(element)
+  // toggle.classList.add("hidden-arrow");
+
+  element.addEventListener("click",function (e) {
+    // toggle.classList.add("hidden-arrow");
+    
+
+    if(element.id=="primero"){
+      document.getElementById("primero").classList.toggle("nav-link2-activate")
+      document.getElementById("segundo").classList.remove("nav-link2-activate")
+      document.getElementById("tercero").classList.remove("nav-link2-activate")
+      document.getElementById("pencil").classList.toggle("icon-active")
+      document.getElementById("clipboard").classList.remove("icon-active")
+      document.getElementById("trash").classList.remove("icon-active")
+      submenu.classList.toggle("activate-sub-menu")
+      primero1.classList.remove("activate-li-sub-menu")
+      submenu1.classList.remove("activate-sub-menu-1")
+
+      // console.log(document.getElementById("pencil"))
+      // console.log(document.getElementById("primero"))
+
+    }
+    else if(element.id=="segundo"){
+      document.getElementById("primero").classList.remove("nav-link2-activate")
+      document.getElementById("segundo").classList.toggle("nav-link2-activate")
+      document.getElementById("tercero").classList.remove("nav-link2-activate")
+      document.getElementById("pencil").classList.remove("icon-active")
+      document.getElementById("clipboard").classList.toggle("icon-active")
+      document.getElementById("trash").classList.remove("icon-active")
+      submenu.classList.remove("activate-sub-menu")
+      primero1.classList.remove("activate-li-sub-menu")
+      submenu1.classList.remove("activate-sub-menu-1")
+
+
+    }
+    else{
+      document.getElementById("primero").classList.remove("nav-link2-activate")
+      document.getElementById("segundo").classList.remove("nav-link2-activate")
+      document.getElementById("tercero").classList.toggle("nav-link2-activate")
+      document.getElementById("pencil").classList.remove("icon-active")
+      document.getElementById("clipboard").classList.remove("icon-active")
+      document.getElementById("trash").classList.toggle("icon-active")
+      submenu.classList.remove("activate-sub-menu")
+      primero1.classList.remove("activate-li-sub-menu")
+      submenu1.classList.remove("activate-sub-menu-1")
+
+    }
+
+    if(element.className.includes("nav-link2-activate")){
+      toggle.classList.add("hidden-arrow")
+    }
+    else{
+      toggle.classList.remove("hidden-arrow");
+    }
+
+    // console.log(element.className)    
   })
 
-
-  // element.addEventListener("mouseout",function(e) {
-  //     toggle.classList.remove("hidden-arrow")
-  //     // console.log(e)
-  // })
+ 
 });
 
-const primero=document.querySelector('.primer');
+// ==================================
+// FUNCIONES DEL PRIMER SUBMENU
+// ============================
+const primero1=document.getElementById("primero_1");
+const primero2=document.getElementsByName("primero_2")
 
-primero.addEventListener("click",()=>{
-  submenu.classList.toggle("activate-sub-menu")
-
-
+primero1.addEventListener("click",function (e){
+  primero1.classList.toggle("activate-li-sub-menu")
+  submenu1.classList.toggle("activate-sub-menu-1")
 })
+
+
 
 
 
