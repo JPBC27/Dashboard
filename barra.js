a838843b62af2c5ec27af1a9c608f1955d415300
Jean Paul <jeanpaulbenitescoico@gmail.com>
2023-03-21 13:21:15 -0500

    21/03-envio

--- barra.js
+++ barra.js
@@ -6,3 +6,17 @@ const body= document.querySelector("body"),
       toggle.addEventListener("click",()=>{
         sidebar.classList.toggle("close"); 
       });
+
+const barraLinks=document.querySelectorAll('.nav-link');
+const textox=document.querySelectorAll('.text');
+
+
+barraLinks.forEach(element => {
+  element.onclick=()=>{
+
+    // console.log(element.parentElement.lastElementChild)
+    
+    document.getElementById(element.lastChild.lastChild.id).classList.toggle("mostrar-texto")
+  }
+
+});
