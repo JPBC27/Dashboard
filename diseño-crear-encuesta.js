18aef12d77e4ee60459584382fb3ef2c3455b298
Jean Paul <jeanpaulbenitescoico@gmail.com>
2023-07-25 17:13:39 -0500

    envio25/07/2023

--- diseño-crear-encuesta.js
+++ diseño-crear-encuesta.js
@@ -90,22 +90,46 @@ primero1.addEventListener("click",function (e){
   submenu1.classList.toggle("activate-sub-menu-1")
 })
 
+
+// ==================================
+// FUNCIONES PARA REMPLAZAR EL FONDO 
+// ============================
+
+const fondo=document.querySelectorAll(".nav-link4");
+const contenedorEncuesta=document.getElementById("contenedor-encuesta")
+const dato3=window.localStorage.prueba
+
+
+fondo.forEach(e => {
+  contenedorEncuesta.style.background=`url(${dato3}) no-repeat center center`;
+  contenedorEncuesta.style.backgroundSize=`100% 100%`;
+
+  e.onclick=()=>{
+    const filename=new URL(e.lastChild.lastChild.src).pathname
+    localStorage.setItem("prueba",JSON.stringify(filename))
+    contenedorEncuesta.style.background=`url(${filename }) no-repeat center center`;
+    contenedorEncuesta.style.backgroundSize=`100% 100%`;
+
+  }
+
+});
+
 // ==================================
 // FUNCIONES DEL DROPDOWN
 // ============================
 
-const optionMenu=document.querySelector(".dropdown"),
-      selectBtn= optionMenu.querySelector(".select-btn"),
-      options= optionMenu.querySelectorAll(".option"),
-      sBtn_text= optionMenu.querySelector(".sBtn-text");
+const optionMenu=document.querySelector(".dropdown-agregar"),
+      selectBtn= optionMenu.querySelector(".select-btn-agregar"),
+      options= optionMenu.querySelectorAll(".option-agregar"),
+      sBtn_text= optionMenu.querySelector(".sBtn-text-agregar");
 
-const menu_multiple= document.getElementById("opcion-multiple"),
-      casilla_verificacion=document.getElementById("opcion-checkbox"),
+const menu_multiple= document.getElementById("opcion-multiple-agregar"),
+      casilla_verificacion=document.getElementById("opcion-checkbox-agregar"),
       texto_simple=document.querySelector(".contendor-texto-simple"),
       control_deslizante=document.querySelector(".contendor-control-deslizante"),
-      contenedor_opcion_multiple=document.getElementById("boton-opcion-multiple"),
-      contenedor_opcion_checkbox=document.getElementById("boton-opcion-checkbox");
-const contenedor_general=document.getElementById("contenedor-opciones")
+      contenedor_opcion_multiple=document.getElementById("boton-opcion-multiple-agregar"),
+      contenedor_opcion_checkbox=document.getElementById("boton-opcion-checkbox-agregar");
+const contenedor_general=document.getElementById("contenedor-opciones-agregar")
 selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
 
       options.forEach(option =>{
@@ -170,37 +194,12 @@ selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"))
 
 
 
-
-
-// ==================================
-// FUNCIONES PARA REMPLAZAR EL FONDO 
-// ============================
-
-  const fondo=document.querySelectorAll(".nav-link4");
-  const contenedorEncuesta=document.getElementById("contenedor-encuesta")
-  const dato3=window.localStorage.prueba
-  
-  
-  fondo.forEach(e => {
-    contenedorEncuesta.style.background=`url(${dato3}) no-repeat center center`;
-    contenedorEncuesta.style.backgroundSize=`100% 100%`;
-  
-    e.onclick=()=>{
-      const filename=new URL(e.lastChild.lastChild.src).pathname
-      localStorage.setItem("prueba",JSON.stringify(filename))
-      contenedorEncuesta.style.background=`url(${filename }) no-repeat center center`;
-      contenedorEncuesta.style.backgroundSize=`100% 100%`;
-
-    }
-  
-  });
-
 // ==================================
 // FUNCIONES PARA  BOTON MAS
 // ============================
 
-const opcion_multiple=document.getElementById("opcion-multiple")
-const mas_opcion_multiple=document.getElementById("mas-opcion-multiple")
+const opcion_multiple=document.getElementById("opcion-multiple-agregar")
+const mas_opcion_multiple=document.getElementById("mas-opcion-multiple-agregar")
 
 // Declara un array para almacenar los objetos con el índice y el valor ingresado en los inputs
 let valoresIngresados = [];
@@ -212,7 +211,7 @@ mas_opcion_multiple.addEventListener("click", () => {
         <input type="radio" name="r" value="clasica" disabled>
         <span></span>
       </label>
-      <input class="input-opciones1" type="text">
+      <input class="input-opciones1 input-opciones1-agregar" type="text">
       <i class="bi bi-dash-circle"></i>
     </div>
   `;
@@ -220,7 +219,7 @@ mas_opcion_multiple.addEventListener("click", () => {
   opcion_multiple.innerHTML += temp;
 
   // Obtiene todos los inputs recién agregados
-  const nuevosInputs = opcion_multiple.querySelectorAll(".input-opciones1");
+  const nuevosInputs = opcion_multiple.querySelectorAll(".input-opciones1-agregar");
 
   // Almacena los valores ingresados en el array de objetos
   nuevosInputs.forEach((input, index) => {
@@ -236,26 +235,12 @@ mas_opcion_multiple.addEventListener("click", () => {
   });
 });
 
+// ==================================
+// FUNCIONES PARA  BOTON MAS CHECKBOX
+// ============================
 
-// mas_opcion_multiple.addEventListener("click",()=>{
-//   // console.log("hola")
-//   const temp=`
-//   <div class="pregunta-opciones" >
-//   <label class="radio1">
-//       <input type="radio" name="r" value="clasica" disabled>
-//       <span></span>
-//   </label>
-//   <input class="input-opciones1" type="text">
-//   <i class="bi bi-dash-circle"></i>
-// </div>
-// `
-//   // opcion_multiple.insertAdjacentHTML("beforeend",temp) =temp
-//   opcion_multiple.innerHTML+=temp;
-// })
-
-
-const opcion_checkbox=document.getElementById("opcion-checkbox")
-const mas_opcion_checkbox=document.getElementById("mas-opcion-checkbox")
+const opcion_checkbox=document.getElementById("opcion-checkbox-agregar")
+const mas_opcion_checkbox=document.getElementById("mas-opcion-checkbox-agregar")
 
 mas_opcion_checkbox.addEventListener("click",()=>{
   // console.log("hola")
@@ -265,7 +250,7 @@ mas_opcion_checkbox.addEventListener("click",()=>{
       <input class="checkbox-spin" type="checkbox" id="check4" disabled/>
       <label for="check4"><span></span></label>
     </div>
-      <input class="input-opciones1" type="text">
+      <input class="input-opciones1 input-opciones1-agregar" type="text">
       <i class="bi bi-dash-circle"></i>
     </div>
 `
@@ -276,11 +261,11 @@ mas_opcion_checkbox.addEventListener("click",()=>{
 // ==================================
 // FUNCIONES BOTON CANCELAR
 // ============================
-const cancelar=document.querySelectorAll(".opcion-cancelar")
-const contenedor_opciones=document.querySelectorAll(".contenedor-opciones")
+const cancelar=document.querySelectorAll(".opcion-cancelar-agregar")
+const contenedor_opciones=document.querySelectorAll(".contenedor-opciones-agregar")
 const contenedor_agregar_pregunta=document.getElementById("contenedor-agregar-pregunta");
-const contenedor_diseño_pregunta=document.getElementById("pregunta")
-const pregunta= document.querySelector(".texto-pregunta")
+const contenedor_diseño_pregunta=document.getElementById("pregunta-agregar")
+const pregunta= document.querySelector(".texto-pregunta-agregar")
 
 cancelar.forEach(element => {
   element.addEventListener("click", () => {
@@ -318,14 +303,14 @@ cancelar.forEach(element => {
 // OBTENER DATOS DE PREGUNTA
 // ============================
 
-const multiples=document.querySelector(".opciones-multiples")
+const multiples=document.querySelector(".opciones-multiples-agregar")
 const guardar=document.getElementById("guardar")
 // const contenedor_agregar_pregunta=document.getElementById("contenedor-agregar-pregunta");
 // const objeto=[];
 
 var objeto = JSON.parse(localStorage.getItem("prueba1")) || [];
 guardar.addEventListener("click",()=>{
-  const respuestas= document.querySelectorAll(".input-opciones1");
+  const respuestas= document.querySelectorAll(".input-opciones1-agregar");
   const values = [];
    
     var question=pregunta.value;
@@ -468,85 +453,111 @@ const mostrar=(id)=>{
   }
 }
 
-// ==================================
+// ================================================================================================
 // FUNCION PARA EDITAR PREGUNTA
+// ================================================================================================
+
+// Cerrar el modalEditar al hacer clic en la "X" o en el fondo oscuro difuminado
+document.getElementById("closeModalEditarButton").addEventListener("click", function() {
+  document.getElementById("modalEditar").style.display = "none";
+});
+
+document.getElementById("modalEditar").addEventListener("click", function(event) {
+  if (event.target === document.getElementById("modalEditar")) {
+    document.getElementById("modalEditar").style.display = "none";
+  }
+});
+
+const editar=(id)=>{
+  document.getElementById("modalEditar").style.display = "block";
+}
+
+
+// ==================================
+// FUNCIONES DEL DROPDOWN EDITAR
 // ==================================
 
-const ocultarContenidoDelDiv = (elemento) => {
+const opcion_multiple_editar=document.getElementById("opcion-multiple")
+const mas_opcion_multiple_editar=document.getElementById("mas-opcion-multiple")
 
-  // Recorre todos los hijos del elemento y oculta su contenido (si los tiene)
-  const hijos = elemento.children;
-  for (let i = 0; i < hijos.length; i++) {
-    // ocultarContenidoDelDiv(hijos[i]);
-    hijos[i].style.display = "none";
+const opcion_checkbox_editar=document.getElementById("opcion-checkbox")
+const mas_opcion_checkbox_editar=document.getElementById("mas-opcion-checkbox")
 
-  }
-};
+const optionMenuEditar=document.querySelector(".dropdown-editar"),
+      selectBtnEditar= optionMenuEditar.querySelector(".select-btn-editar"),
+      optionsEditor= optionMenuEditar.querySelectorAll(".option-editar"),
+      sBtn_text_editar= optionMenuEditar.querySelector(".sBtn-text-editar");
 
-const editar = (id) => {
-  let contPregunta = document.getElementById(id.id);
-  contPregunta.style.display = "none";
-  contenedor_diseño_pregunta.style.display = "block";
-  const preguntaId = id.id; // El id de la pregunta que se está editando
-  const objetoGuardado = JSON.parse(localStorage.getItem("prueba1"));
+const menu_multiple_editar= document.getElementById("opcion-multiple-editar"),
+      casilla_verificacion_editar=document.getElementById("opcion-checkbox-editar"),
+      texto_simple_editar=document.querySelector(".contendor-texto-simple"),
+      control_deslizante_editar=document.querySelector(".contendor-control-deslizante"),
+      contenedor_opcion_multiple_editar=document.getElementById("boton-opcion-multiple-editar"),
+      contenedor_opcion_checkbox_editar=document.getElementById("boton-opcion-checkbox-editar");
+const contenedor_general_editar=document.getElementById("contenedor-opciones-editar")
+selectBtnEditar.addEventListener("click", () => optionMenuEditar.classList.toggle("active"));
 
-  // Busca el objeto correspondiente al id de la pregunta en el array objetoGuardado
-  const preguntaEditada = objetoGuardado.find((objeto) => objeto.id === preguntaId);
+      options.forEach(option =>{
+        option.addEventListener("click", ()=>{
+          let selectedOption=option.querySelector(".option-text-editar").innerText;
+          sBtn_text_editar.innerText= selectedOption;
+          
+          optionMenuEditar.classList.remove("active");
+          // console.log(selectedOption)
 
-  if (preguntaEditada) {
-    // Si se encontró el objeto, asigna los valores almacenados a los campos del formulario
-    let preguntaInput = document.querySelector(".texto-pregunta");
-    let botonTipo = document.querySelector(".sBtn-text");
-    let opcionesMultiplesContainer = document.querySelector(".opciones-multiples");
-    
-    // Verifica que los elementos se han encontrado correctamente antes de asignar valores
-    if (preguntaInput && preguntaEditada.pregunta) {
-      preguntaInput.value = preguntaEditada.pregunta;
-    }
+          // ==================================
+          // FUNCIONES PARA OPCIONES DE PREGUNTA
+          // =====================================
 
-    if (botonTipo && preguntaEditada.tipo) {
-      botonTipo.innerText = preguntaEditada.tipo;
-    }
+          if(selectedOption=="Opción múltiples"){
+            contenedor_general_editar.style.display="block"
+            menu_multiple_editar.style.display ="block";
+            casilla_verificacion_editar.style.display ="none";
+            // texto_simple.sty le.display="none";
+            // control_deslizante.style.display="none";
+            opcion_checkbox_editar.innerHTML=''
+            contenedor_opcion_multiple_editar.style.display="flex"
+            contenedor_opcion_checkbox_editar.style.display="none"
+          }
 
-    if (preguntaEditada.tipo === "Opción múltiples") {
-      // Mostrar campos adicionales y cargar las opciones múltiples desde el objetoGuardado
+          else if(selectedOption=="Casillas de verificación"){
+            contenedor_general_editar.style.display="block"
 
-      // Borra los campos previamente creados (si los hay) para evitar duplicados
-      opcionesMultiplesContainer.innerHTML = "";
+            menu_multiple.style_editar.display ="none";
+            casilla_verificacion_editar.style.display ="block";
+            // texto_simple.style.display="none";
+            // control_deslizante.style.display="none";
+            opcion_multiple_editar.innerHTML=''
+            contenedor_opcion_multiple_editar.style.display="none"
+            contenedor_opcion_checkbox_editar.style.display="flex"
+          }
+          else if(selectedOption=="Texto simple"){
+            menu_multiple_editar.style.display ="none";
+            casilla_verificacion_editar.style.display ="none";
+            // texto_simple.style.display="block";
+            // control_deslizante.style.display="none";
+            opcion_checkbox_editar.innerHTML=''
+            opcion_multiple_editar.innerHTML=''
+            contenedor_opcion_multiple_editar.style.display="none"
+            contenedor_opcion_checkbox_editar.style.display="none"
+          }
+          else {
+            menu_multiple_editar.style.display ="none";
+            casilla_verificacion_editar.style.display ="none";
+            // texto_simple.style.display="none";
+            // control_deslizante.style.display="block";
+            opcion_checkbox_editar.innerHTML=''
+            opcion_multiple_editar.innerHTML=''
+            contenedor_opcion_multiple_editar.style.display="none"
+            contenedor_opcion_checkbox_editar.style.display="none"
+          }
 
-      // Recorre las respuestas del objetoGuardado y crea los campos de opciones múltiples
-      preguntaEditada.respuestas.forEach((respuesta) => {
-        const nuevaOpcion = document.createElement("div");
-        nuevaOpcion.classList.add("pregunta-opciones");
-        nuevaOpcion.innerHTML = `
-          <label class="radio1">
-            <input type="radio" name="r" value="clasica" disabled>
-            <span></span>
-          </label>
-          <input class="input-opciones1" type="text" value="${respuesta}">
-          <i class="bi bi-dash-circle"></i>
-        `;
-        opcionesMultiplesContainer.appendChild(nuevaOpcion);
-      });
+        })
+      })
 
-      // Mostrar el contenedor de opciones múltiples
-      opcionesMultiplesContainer.style.display = "block";
 
-    } else if (preguntaEditada.tipo === "Casillas de verificación") {
-      // Mostrar campos adicionales y cargar las casillas de verificación desde el objetoGuardado
-      // ...
-
-    } else if (preguntaEditada.tipo === "Texto simple") {
-      // Mostrar campos adicionales y cargar el texto simple desde el objetoGuardado
-      // ...
-
-    } else if (preguntaEditada.tipo === "Control deslizante") {
-      // Mostrar campos adicionales y cargar los valores del control deslizante desde el objetoGuardado
-      // ...
-    }
-  }
-};
-
+// ================================================================================================
+// ================================================================================================
 
 // ==================================
 // FUNCION PARA ELIMINAR PREGUNTA
@@ -610,3 +621,4 @@ boton_agregar_pregunta.addEventListener("click",()=>{
 
 
 renderQuestions();
+
